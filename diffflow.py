c8638a420d278cb28903183a73d169065a25b544
Ilana Bromberg <isbromberg@gmail.com>
2025-11-12 13:40:39 -0500

    Update diffflow.py
    cleaned up

--- diffflow.py
+++ diffflow.py
@@ -8,14 +8,17 @@ Created on Mon Oct 23 12:19:51 2023
 creating my own differential velocity magnitude file
 
 theta is long, phi is lat
+
+use datagrid.py to get the velocities along a regular grid
+
+big change apr 2025: add color map for vertical velocity (average values at surf and 28bsl?)
 """
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patheffects as pe
-
-from matplotlib import cm
+from matplotlib import colors
+from matplotlib.cm import ScalarMappable
 
 def cart2sph(x,y,z): # function for converting cartesian coords to spherical
     theta = np.arctan2(y,x) * 180/np.pi # azimuth
@@ -28,8 +31,10 @@ def cart2sphV(long,lat,vx,vy,vz): # convert velocities to spherical
     theta = long * np.pi/180
     phi = [90-(i * np.pi/180) for i in lat] 
 
-    #vr = vx * np.sin(phi) * np.cos(theta) + vy * np.sin(phi) * np.sin(theta) + vz * np.cos(phi)
-    vphi = vx * np.cos(phi) * np.cos(theta) + vy * np.cos(phi) * np.sin(theta) - vz * np.sin(phi)
+    # vr = ( vx * np.sin(phi) * np.cos(theta) + vy * np.sin(phi) 
+    #         * np.sin(theta) + vz * np.cos(phi) )
+    vphi = ( vx * np.cos(phi) * np.cos(theta) + vy * np.cos(phi) 
+            * np.sin(theta) - vz * np.sin(phi) )
     vtheta = -vx * np.sin(theta) + vy * np.cos(theta)
     
     vLong = vtheta
@@ -60,13 +65,45 @@ def norm(vLong,vLat):
 
 # -------------------------------------------------------
 
+# read in data from comsol: topography, velocity topo, and velocity 28km bsl
+# paste in pathnames
+
+
+#yr = "17"
+fcm = "5"
+lc = "21"
+
+#topo_str = "/Users/ibromberg/Documents/COMSOL 61/extrusion/text_outputs/fcm"+fcm+"lc"+lc+"_topo_"+yr+"ma.txt"
+#v_topo_str = "/Users/ibromberg/Documents/COMSOL 61/extrusion/text_outputs/fcm"+fcm+"lc"+lc+"_v_topo_"+yr+"ma.txt"
+#v_28_str = "/Users/ibromberg/Documents/COMSOL 61/extrusion/text_outputs/fcm"+fcm+"lc"+lc+"_v_28bsl_"+yr+"ma.txt"
+#v_normal = "/Users/ibromberg/Documents/COMSOL 61/extrusion/text_outputs/fcm"+fcm+"lc"+lc+"_v_normal_"+yr+"ma.txt"
+
 # read in data from comsol: topography, velocity topo, and velocity 28kmbsl
-topo = pd.read_csv('fcm5_lc22_isostacy_noslip_bottom.txt',sep=" ",
-                   header=None,names=["x","y","z","elev"])
-vtopo = pd.read_csv('fcm2_nobc_isotest_wallnoslip_bottomconstraint_topov.txt',
+timestep = "17_0" # file name
+timestep_t = "17.00"
+path = '/Users/ibromberg/Documents/COMSOL 63/velocitydata/fcm5lc21_'
+savepath = '/Users/ibromberg/Documents/COMSOL 63/plots/'
+
+#'fcm2_nobc_isostacytest_wallsnoslip_bottomconstraint/v28km'+timestep+'.txt'
+
+vtopo = pd.read_csv(path+'vtopo'+timestep+'.txt',
                     sep=" ",comment='%',header=None,names=["x","y","z","vx","vy","vz"])
-v28 = pd.read_csv('fcm2_nobc_isotest_wallnoslip_bottomconstraint_28kmv.txt',
+v28 = pd.read_csv(path+'v28km'+timestep+'.txt',
                   sep=" ",comment='%',header=None,names=["x","y","z","vx","vy","vz"])
+vnorm = pd.read_csv(path+'vnorm'+timestep+'.txt',
+                  sep=" ",comment='%',header=None,names=["x","y","z","vn"])
+
+# paste pathname here
+savepath = '/Users/ibromberg/Documents/COMSOL 63/plots/diffv/'
+
+#topo = pd.read_csv(topo_str ,sep=" ",
+#                   header=None,names=["x","y","z","elev"])
+#vtopo = pd.read_csv(vtopo,
+#                    sep=" ",comment='%',header=None,names=["x","y","z","vx","vy","vz"])
+#v28 = pd.read_csv(v28,
+#                  sep=" ",comment='%',header=None,names=["x","y","z","vx","vy","vz"])
+#vnorm = pd.read_csv(v_normal,
+#                  sep=" ",comment='%',header=None,names=["x","y","z","vn"])
 
 # read in the states data
 arizona = pd.read_csv('deformed_state_boundaries/arizona_17.0.dat', sep = ' ', header=None)
@@ -87,10 +124,16 @@ core = pd.read_csv('corecomplex.txt', sep=" ", header=None, comment='%',
 # ------------------------------------------
 
 # arrays for surface topography
-x, y, z = (np.array(topo["x"].values.tolist()), 
-           np.array(topo["y"].values.tolist()), 
-           np.array(topo["z"].values.tolist()))
-theta, phi, r = cart2sph(x,y,z)
+#x, y, z = (np.array(topo["x"].values.tolist()), 
+#           np.array(topo["y"].values.tolist()), 
+#           np.array(topo["z"].values.tolist()))
+#theta, phi, r = cart2sph(x,y,z)
+
+xnorm, ynorm, znorm, v_norm = (np.array(vnorm["x"].values.tolist()), 
+           np.array(vnorm["y"].values.tolist()), 
+           np.array(vnorm["z"].values.tolist()),
+           np.array(vnorm["vn"].values.tolist()) )
+
 
 # arrays for surface XYZ coords
 xsurf, ysurf, zsurf = (np.array(vtopo["x"].values.tolist()), 
@@ -121,6 +164,12 @@ vLong28, vLat28 = cart2sphV(theta28,phi28,vx28,vy28,vz28)
 vLongdiff = np.array(diff(vLongsurf,vLong28))
 vLatdiff = np.array(diff(vLatsurf,vLat28))
 
+vLongdiff = np.array(diff(vLong28,vLongsurf))
+vLatdiff = np.array(diff(vLat28,vLatsurf))
+
+#vLongdiff = np.mean( np.array([ vLongsurf, vLong28]), axis=0 )
+#vLatdiff = np.mean( np.array([ vLatsurf, vLat28]), axis=0 )
+
 # core complexes
 cclong = core['long']#.values.tolist()
 cclat = core['lat']#.values.tolist()
@@ -132,73 +181,124 @@ ccdeg = core['deg']
 f, ax = plt.subplots(1)
 f.set_size_inches(12, 10)
 
+scaling = 5e-9
+
+#plt.tricontourf(thetasurf,phisurf,v_norm,levels=50,cmap="PRGn")
+#plt.colorbar(label="mm/yr")
+
 # surface and 28km bsl velocities
 #plt.tricontourf(theta,phi,r,cmap="gist_earth") # topography
-Q1 = plt.quiver(thetasurf,phisurf, vLongsurf, vLatsurf,color='royalblue',label="Surface Velocity") # surface arrows
-Q2 = plt.quiver(theta28,phi28, vLong28, vLat28,color='firebrick',label="28 km BSL Velocity") # surface arrows
+# add scale=1e-9 to get them both on the same plotting scale
+Q2 = plt.quiver(theta28,phi28, vLong28, vLat28,color='royalblue',label="28 km BSL Velocity",scale=scaling) # surface arrows
+Q1 = plt.quiver(thetasurf,phisurf, vLongsurf, vLatsurf,color='firebrick',label="Surface Velocity",scale=scaling) # surface arrows
 
-plt.quiverkey(Q1,0.3,0.3,3.171e-11,label="1 mm/yr",coordinates='figure')
-plt.quiverkey(Q2,0.3,0.25,3.171e-11,label="1 mm/yr",coordinates='figure')
+
+plt.quiverkey(Q1,0.2,0.35,3.171e-10,label="10 mm/yr",coordinates='figure',color='black')
+plt.quiverkey(Q1,0.2,0.3,3.171e-10/2,label="5 mm/yr",coordinates='figure',color='black')
+plt.quiverkey(Q1,0.2,0.25,3.171e-10/10,label="1 mm/yr",coordinates='figure',color='black')
+
+# Q1 = plt.quiver(thetasurf,phisurf, vLongsurf, vLatsurf,color='royalblue',label="Surface Velocity",scale=0.5e-8) # surface arrows
+# Q2 = plt.quiver(theta28,phi28, vLong28, vLat28,color='firebrick',label="28 km BSL Velocity",scale=0.5e-8) # surfa,ce arrows
+
+# plt.quiverkey(Q1,0.3, 0.3, 3.171e-10,label="10 mm/yr",coordinates='figure')
+# plt.quiverkey(Q2,0.3, 0.25, 3.171e-10,label="10 mm/yr",coordinates='figure')
 
 plt.xlim([-122.5,-105])
 plt.ylim([28,42])
-plt.title("Surface Flow")
+plt.title("Velocities: "+timestep_t+" Ma",fontsize=30)
+
+statecolor='black'
+a=0.5
 
 # add the deformed state lines
-plt.plot(arizona[0],arizona[1], color = "black")
-plt.plot(california[0],california[1], color = "black")
-plt.plot(colorado[0],colorado[1], color = "black")
+plt.plot(arizona[0],arizona[1], color = statecolor, alpha=a)
+plt.plot(california[0],california[1], color = statecolor, alpha=a)
+plt.plot(colorado[0],colorado[1], color =  statecolor, alpha=a)
 #plt.plot(mexico[0],mexico[1], color = "black")
-plt.plot(nevada[0],nevada[1], color = "black")
-plt.plot(new_mexico[0],new_mexico[1], color = "black")
-plt.plot(oregon[0],oregon[1], color = "black")
-plt.plot(texas[0],texas[1], color = "black")
-plt.plot(utah[0],utah[1], color = "black")
-plt.plot(wyoming[0],wyoming[1], color = "black")
+plt.plot(nevada[0],nevada[1], color =  statecolor, alpha=a)
+plt.plot(new_mexico[0],new_mexico[1], color =  statecolor, alpha=a)
+plt.plot(oregon[0],oregon[1], color =  statecolor, alpha=a)
+plt.plot(texas[0],texas[1], color =  statecolor, alpha=a)
+plt.plot(utah[0],utah[1], color = statecolor, alpha=a)
+plt.plot(wyoming[0],wyoming[1], color =  statecolor, alpha=a)
 
 # plot core complexes
-# dots = plt.scatter(mcc[0],mcc[1],marker='o',s=150,color="grey",edgecolors="black",linewidth=1,zorder=100)
-plotcc = plt.scatter(cclong,cclat,marker='o',s=100,color='indigo',label="Core Complexes")
 
+# plot core complexes
 for i in range(0,len(ccdeg)):
     #draw_line(cclong[i],cclat[i],ccdeg[i],10)
     long_end, lat_end = draw_line(cclat[i],cclong[i],ccdeg[i],1)
-    plt.plot([cclong[i], long_end],[cclat[i],lat_end],color = "indigo",linewidth=3)
+    plt.plot([cclong[i], long_end],[cclat[i],lat_end],color = "k",linewidth=4.5, zorder=10)
+    plt.plot([cclong[i], long_end],[cclat[i],lat_end],color = "magenta",linewidth=3, zorder=10)
+
+# dots = plt.scatter(mcc[0],mcc[1],marker='o',s=150,color="grey",edgecolors="black",linewidth=1,zorder=100)
+plotcc = plt.scatter(cclong,cclat,marker='s',s=100,color='magenta',edgecolors='k',label="Core Complexes",zorder=11)
+
 
 plt.legend()
-plt.savefig("poster/surfand28v.png",dpi=300,bbox_inches='tight')
+plt.savefig(savepath + "surfand28v" + timestep_t + ".png",dpi=300,bbox_inches='tight')
 #plt.show()
 
+#plt.xlim([-112,-106])
+#plt.ylim([34,40])
+#plt.savefig(savepath1,dpi=300,bbox_inches='tight')
+
 # differential velocities ---------------------------------------------
+
 f, ax = plt.subplots(1)
 f.set_size_inches(12, 10)
 
-Q3 = plt.quiver(thetasurf,phisurf, vLongdiff, vLatdiff,color='royalblue',label="Differential Velocity") # surface arrows
-plt.quiverkey(Q3,0.3,0.3,3.171e-11,label="1 mm/yr",coordinates='figure')
+
+# COMMENT BELOW OUT if you want different scaled color bars for each time step
+normalcolor = colors.TwoSlopeNorm(vmin=-5,vcenter=0,vmax=5) 
+
+plt.tricontourf(thetasurf,phisurf,v_norm,levels=50,cmap="RdBu_r",norm=normalcolor)
+#plt.colorbar(label="Surface Vertical Velocity (mm/yr)")
+
+sm = ScalarMappable(norm=normalcolor, cmap='RdBu_r')
+sm.set_array([])  # Dummy array to satisfy matplotlib
+
+cbar=plt.colorbar(sm,ax=ax,label="Surface Vertical Velocity (mm/yr)")
+
+# 8e-9 works for scaling too
+scaling = 2e-9
+
+Q3 = plt.quiver(thetasurf,phisurf, vLongdiff, vLatdiff,color='black',scale=scaling,label="Differential Velocity",zorder=12) # surface arrows
+plt.quiverkey(Q3,0.2,0.3,3.171e-10,label="10 mm/yr",coordinates='figure')
+plt.quiverkey(Q3,0.2,0.25,3.171e-10/2,label="5 mm/yr",coordinates='figure')
+plt.quiverkey(Q3,0.2,0.2,3.171e-10/10,label="1 mm/yr",coordinates='figure')
 
 plt.xlim([-122.5,-105])
 plt.ylim([28,42])
-plt.title("Differential Velocity")
+plt.title("Differential Velocities: "+timestep_t+" Ma",fontsize=30)
 
 # add the deformed state lines
-plt.plot(arizona[0],arizona[1], color = "black")
-plt.plot(california[0],california[1], color = "black")
-plt.plot(colorado[0],colorado[1], color = "black")
+
+plt.plot(arizona[0],arizona[1], color = statecolor,alpha=a)
+plt.plot(california[0],california[1], color = statecolor,alpha=a)
+plt.plot(colorado[0],colorado[1], color = statecolor,alpha=a)
 #plt.plot(mexico[0],mexico[1], color = "black")
-plt.plot(nevada[0],nevada[1], color = "black")
-plt.plot(new_mexico[0],new_mexico[1], color = "black")
-plt.plot(oregon[0],oregon[1], color = "black")
-plt.plot(texas[0],texas[1], color = "black")
-plt.plot(utah[0],utah[1], color = "black")
-plt.plot(wyoming[0],wyoming[1], color = "black")
+plt.plot(nevada[0],nevada[1], color = statecolor,alpha=a)
+plt.plot(new_mexico[0],new_mexico[1], color = statecolor,alpha=a)
+plt.plot(oregon[0],oregon[1], color = statecolor,alpha=a)
+plt.plot(texas[0],texas[1], color = statecolor,alpha=a)
+plt.plot(utah[0],utah[1], color = statecolor,alpha=a)
+plt.plot(wyoming[0],wyoming[1], color = statecolor,alpha=a)
 
 # plot core complexes
-# dots = plt.scatter(mcc[0],mcc[1],marker='o',s=150,color="grey",edgecolors="black",linewidth=1,zorder=100)
-plotcc = plt.scatter(cclong,cclat,marker='o',s=100,color='indigo',label="Core Complexes")
-
 for i in range(0,len(ccdeg)):
     #draw_line(cclong[i],cclat[i],ccdeg[i],10)
     long_end, lat_end = draw_line(cclat[i],cclong[i],ccdeg[i],1)
-    plt.plot([cclong[i], long_end],[cclat[i],lat_end],color = "indigo",linewidth=3)
+    plt.plot([cclong[i], long_end],[cclat[i],lat_end],color = "k",linewidth=4.5, zorder=10)
+    plt.plot([cclong[i], long_end],[cclat[i],lat_end],color = "magenta",linewidth=3, zorder=10)
 
-plt.savefig("poster/diffv.png",dpi=300,bbox_inches='tight')
+# dots = plt.scatter(mcc[0],mcc[1],marker='o',s=150,color="grey",edgecolors="black",linewidth=1,zorder=100)
+plotcc = plt.scatter(cclong,cclat,marker='s',s=100,color='magenta',edgecolors='k',label="Core Complexes",zorder=11)
+
+plt.legend()
+
+plt.savefig(savepath + "diffv" + timestep_t + ".png",dpi=300,bbox_inches='tight')
+
+#zoom in on CC:
+#plt.xlim([-116,-112])
+#plt.ylim([33,36])
